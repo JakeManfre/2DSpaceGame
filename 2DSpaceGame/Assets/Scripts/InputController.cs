25b36b7abadf0a24f1e26fa1aa83a6a82ebe4480
Jake Manfre <jrmanfre@mac.com>
2018-12-24 16:04:27 -0500

    Added input controller script

---
+++ 2DSpaceGame/Assets/Scripts/InputController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    //public static InputController Instance { set; get; }
+
+    public static event EventHandler<InputInfo> HorizontalInput;
+    public static event EventHandler<InputInfo> VerticalInput;
+
+    float horizontal;
+    float vertical;
+
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instance = this;
+        speed = 2; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        // move right
+        if (horizontal > 0)
+        {
+            HorizontalInput?.Invoke(this, new InputInfo(horizontal));
+        }
+        // move left
+        if (horizontal < 0)
+        { 
+        HorizontalInput?.Invoke(this, new InputInfo(horizontal));
+        }
+        // move up
+        if (vertical > 0)
+        {
+            VerticalInput?.Invoke(this, new InputInfo(vertical));
+        }
+        // move down
+        if (vertical < 0)
+        {
+            VerticalInput?.Invoke(this, new InputInfo(vertical));
+        }
+    }
+}
+
+public class InputInfo : EventArgs
+{
+    public float value;
+
+    public InputInfo(float val)
+    {
+        value = val;
+    }
+}
--- 2DSpaceGame/Assets/Scripts/SpaceshipController.cs
+++ 2DSpaceGame/Assets/Scripts/SpaceshipController.cs
@@ -1,44 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceshipController : MonoBehaviour
 {
-    float horizontal;
-    float vertical;
-
-    float speed;
+    [SerializeField]
+    private readonly InputController _inputController;
+    [SerializeField]
+    float speed = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 2;
+        InputController.HorizontalInput += InputController_HorizontalInput;
+        InputController.VerticalInput += InputController_VerticalInput;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Called when horizontal input event is called
+    /// </summary>
+    /// <param name="sender">Sender.</param>
+    /// <param name="e">E.</param>
+    private void InputController_HorizontalInput(object sender, InputInfo e)
     {
-        // FIXME: Move all input to a seperate input manager script!
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        // move right
-        if (horizontal > 0)
+        // right input
+        if (e.value > 0)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
         }
-        // move left
-        if (horizontal < 0)
+        // left input
+        else if (e.value < 0)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
         }
-        // move up
-        if (vertical > 0)
+    }
+
+    /// <summary>
+    /// Called when vertical input event is called
+    /// </summary>
+    /// <param name="sender">Sender.</param>
+    /// <param name="e">E.</param>
+    private void InputController_VerticalInput(object sender, InputInfo e)
+    {
+        // up input
+        if (e.value > 0)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, speed));
         }
-        // move down
-        if (vertical < 0)
+        // down input
+        else if (e.value < 0)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -speed));
         }
