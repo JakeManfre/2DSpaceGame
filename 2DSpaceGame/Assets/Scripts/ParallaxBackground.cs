8744883af21861880b3ca7f3ec9367f5a5aaa801
Jake Manfre <jrmanfre@mac.com>
2018-12-23 21:58:28 -0500

    Added parallaxing background framework
    Framework in place to add infinite scrolling parallax with multiple layers.
    
    Also added basic player controls. This will be updated to a more official system later.

---
+++ 2DSpaceGame/Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _playerShip;
+    [SerializeField]
+    [Header("Parallax rate must be greater than 0.")]
+    private int _parallaxRate;
+
+    private Vector2 _initialOffset;
+
+    private float _horizontalParallax;
+    private float _verticalParallax;
+
+    Material _starfieldMat;
+    string _texture;
+
+    float _screenWidth;
+    const float _backgroundBuffer = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_parallaxRate == 0)
+            Debug.LogError("Parallax rate must be greater than 0!");
+
+        _starfieldMat = GetComponent<Renderer>().material;
+        _texture = "_MainTex";
+
+        _initialOffset = _starfieldMat.GetTextureOffset(_texture);
+
+        _screenWidth = (Camera.main.orthographicSize * Screen.width / Screen.height) * 2;
+
+        transform.localScale = new Vector2(_screenWidth * _backgroundBuffer, _screenWidth * _backgroundBuffer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // set texture position to ships positoin
+        transform.position = _playerShip.position;
+
+        // calculate and set texture offset based on the ships position
+        _horizontalParallax = _playerShip.position.x / _parallaxRate;
+        _verticalParallax = _playerShip.position.y / _parallaxRate;
+
+        _starfieldMat.SetTextureOffset(_texture, _initialOffset + new Vector2(_horizontalParallax, _verticalParallax));
+    }
+}
---
+++ 2DSpaceGame/Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // FIXME: Move all input to a seperate input manager script!
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        // move right
+        if (horizontal > 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
+        }
+        // move left
+        if (horizontal < 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+        }
+        // move up
+        if (vertical > 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, speed));
+        }
+        // move down
+        if (vertical < 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -speed));
+        }
+    }
+}
