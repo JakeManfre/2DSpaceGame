d6a9a828a4ba47e45aeceeb6f885eb5127778d73
Jake Manfre <jrmanfre@mac.com>
2018-12-25 16:06:21 -0500

    Updated ship controller so the ship rotates in the direction it is moving.

--- 2DSpaceGame/Assets/Scripts/InputController.cs
+++ 2DSpaceGame/Assets/Scripts/InputController.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
-    //public static InputController Instance { set; get; }
+    public static event EventHandler<InputInfo> LeftInput;
+    public static event EventHandler<InputInfo> RightInput;
+    public static event EventHandler<InputInfo> UpInput;
+    public static event EventHandler<InputInfo> DownInput;
+    public static event EventHandler ClearHorizontalInput;
+    public static event EventHandler ClearVerticalInput;
 
-    public static event EventHandler<InputInfo> HorizontalInput;
-    public static event EventHandler<InputInfo> VerticalInput;
+    private float horizontal;
+    private float vertical;
 
-    float horizontal;
-    float vertical;
+    private float speed;
 
-    float speed;
-
+    /*---------------------------------------------------------------------------------*/
     // Start is called before the first frame update
     void Start()
     {
-        //Instance = this;
         speed = 2; 
     }
-
+    /*---------------------------------------------------------------------------------*/
     // Update is called once per frame
     void Update()
     {
@@ -31,26 +33,54 @@ public class InputController : MonoBehaviour
         // move right
         if (horizontal > 0)
         {
-            HorizontalInput?.Invoke(this, new InputInfo(horizontal));
+            RightInput?.Invoke(this, new InputInfo(horizontal));
         }
         // move left
         if (horizontal < 0)
         { 
-        HorizontalInput?.Invoke(this, new InputInfo(horizontal));
+            LeftInput?.Invoke(this, new InputInfo(horizontal));
         }
+        // left key up
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            //Debug.Log("left key up");
+            ClearHorizontalInput?.Invoke(this, new EventArgs());
+        }
+        // right key up
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            //Debug.Log("right key up");
+            ClearHorizontalInput?.Invoke(this, new EventArgs());
+        }
+
+
         // move up
         if (vertical > 0)
         {
-            VerticalInput?.Invoke(this, new InputInfo(vertical));
+            UpInput?.Invoke(this, new InputInfo(vertical));
         }
         // move down
         if (vertical < 0)
         {
-            VerticalInput?.Invoke(this, new InputInfo(vertical));
+            DownInput?.Invoke(this, new InputInfo(vertical));
+        }
+        // up key up
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            //Debug.Log("up key up");
+            ClearVerticalInput?.Invoke(this, new EventArgs());
+        }
+        // down key up
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            //Debug.Log("down key up");
+            ClearVerticalInput?.Invoke(this, new EventArgs());
         }
     }
 }
-
+/*---------------------------------------------------------------------------------*/
+/*---------------- class of input info to pass in event calls ---------------------*/
+/*---------------------------------------------------------------------------------*/
 public class InputInfo : EventArgs
 {
     public float value;
--- 2DSpaceGame/Assets/Scripts/SpaceshipController.cs
+++ 2DSpaceGame/Assets/Scripts/SpaceshipController.cs
@@ -6,52 +6,143 @@ using UnityEngine;
 public class SpaceshipController : MonoBehaviour
 {
     [SerializeField]
-    private readonly InputController _inputController;
-    [SerializeField]
-    float speed = 2;
+    private Camera[] _cameras;
+    private Vector3 _playerCamPos;
 
+    [SerializeField]
+    private float _speed = 2;
+
+    [SerializeField]
+    private bool _showShipsVector;
+
+    private float _horizontalMag;
+    private float _verticalMag;
+
+    private Vector3 _rotation;
+
+    private LineRenderer _line;
+
+    private Vector3 _shipsForwardPoint;
+
+    /*---------------------------------------------------------------------------------*/
     // Start is called before the first frame update
     void Start()
     {
-        InputController.HorizontalInput += InputController_HorizontalInput;
-        InputController.VerticalInput += InputController_VerticalInput;
-    }
+        InputController.RightInput += InputController_RightInput;
+        InputController.LeftInput += InputController_LeftInput;
+        InputController.UpInput += InputController_UpInput;
+        InputController.DownInput += InputController_DownInput;
+        InputController.ClearHorizontalInput += InputController_ClearHorizontalInput;
+        InputController.ClearVerticalInput += InputController_ClearVerticalInput;
 
+        _playerCamPos = _cameras[0].transform.position;
+        _rotation = Vector3.zero;
+        _shipsForwardPoint = transform.position + transform.up;
+
+        _line = GetComponent<LineRenderer>();
+        _line.enabled = _showShipsVector;
+
+        gameObject.transform.GetChild(0).eulerAngles = new Vector3(90, 0, 0);
+        transform.LookAt(_shipsForwardPoint);
+    }
+    /*---------------------------------------------------------------------------------*/
+    // updates every frame
+    private void Update()
+    {
+        // set camera position to follow the players ship
+        _playerCamPos.x = transform.position.x;
+        _playerCamPos.y = transform.position.y;
+
+        // set each cameras position to the ships position
+        foreach (Camera cam in _cameras)
+        {
+            cam.transform.position = _playerCamPos;
+        }
+
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+
+        _shipsForwardPoint = transform.position + new Vector3(velocity.x, velocity.y, 0);
+
+        // if show ships vector bool is checked in the inspector, show the vector visually
+        if (_showShipsVector)
+        {
+            _line.SetPosition(0, transform.position);
+            _line.SetPosition(1, _shipsForwardPoint);
+        }
+
+        transform.LookAt(_shipsForwardPoint, transform.up);
+    }
+    /*---------------------------------------------------------------------------------*/
     /// <summary>
-    /// Called when horizontal input event is called
+    /// Inputs the controller right input.
     /// </summary>
     /// <param name="sender">Sender.</param>
     /// <param name="e">E.</param>
-    private void InputController_HorizontalInput(object sender, InputInfo e)
+    private void InputController_RightInput(object sender, InputInfo e)
     {
-        // right input
-        if (e.value > 0)
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
-        }
-        // left input
-        else if (e.value < 0)
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
-        }
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(_speed, 0));
     }
-
+    /*---------------------------------------------------------------------------------*/
     /// <summary>
-    /// Called when vertical input event is called
+    /// Inputs the controller left input.
     /// </summary>
     /// <param name="sender">Sender.</param>
     /// <param name="e">E.</param>
-    private void InputController_VerticalInput(object sender, InputInfo e)
+    private void InputController_LeftInput(object sender, InputInfo e)
     {
-        // up input
-        if (e.value > 0)
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, speed));
-        }
-        // down input
-        else if (e.value < 0)
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -speed));
-        }
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-_speed, 0));
+    }
+    /*---------------------------------------------------------------------------------*/
+    /// <summary>
+    /// Inputs the controller clear horizontal input.
+    /// </summary>
+    /// <param name="sender">Sender.</param>
+    /// <param name="e">E.</param>
+    private void InputController_ClearHorizontalInput(object sender, EventArgs e)
+    {
+        _verticalMag = 0;
+    }
+    /*---------------------------------------------------------------------------------*/
+    /// <summary>
+    /// Inputs the controller up input.
+    /// </summary>
+    /// <param name="sender">Sender.</param>
+    /// <param name="e">E.</param>
+    private void InputController_UpInput(object sender, InputInfo e)
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, _speed));
+    }
+    /*---------------------------------------------------------------------------------*/
+    /// <summary>
+    /// Inputs the controller down input.
+    /// </summary>
+    /// <param name="sender">Sender.</param>
+    /// <param name="e">E.</param>
+    private void InputController_DownInput(object sender, InputInfo e)
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -_speed));
+    }
+    /*---------------------------------------------------------------------------------*/
+    /// <summary>
+    /// Inputs the controller clear vertical input.
+    /// </summary>
+    /// <param name="sender">Sender.</param>
+    /// <param name="e">E.</param>
+    private void InputController_ClearVerticalInput(object sender, EventArgs e)
+    {
+        _horizontalMag = 0;
+    }
+    /*---------------------------------------------------------------------------------*/
+    /// <summary>
+    /// Unsubscribe methods from events on destroy.
+    /// </summary>
+    private void OnDestroy()
+    {
+        InputController.RightInput -= InputController_RightInput;
+        InputController.LeftInput -= InputController_LeftInput;
+        InputController.UpInput -= InputController_UpInput;
+        InputController.DownInput -= InputController_DownInput;
+        InputController.ClearHorizontalInput -= InputController_ClearHorizontalInput;
+        InputController.ClearVerticalInput -= InputController_ClearVerticalInput;
     }
 }
